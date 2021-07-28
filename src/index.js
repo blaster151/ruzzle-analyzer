1d372358b7008138ff733789822a373d12b3b546
Jeff Benson <jeff.benson@docusign.com>
2021-07-28 14:27:49 -0500

    Basic Ruzzle generator and analyzer

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { Component } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+console.log('at top of index.js');
+render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
